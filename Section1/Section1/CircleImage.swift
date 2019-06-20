23640586eb57986985a7788396546081e5190ef4
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-06-20 18:46:48 +0900

    add CircleImage

---
+++ Section1/Section1/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Section1
+//
+//  Created by 小幡 十矛 on 2019/06/20.
+//  Copyright © 2019 Tomu Obata. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
